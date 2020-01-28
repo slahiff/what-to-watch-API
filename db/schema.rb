0c66b21e9e342af4de0cda5249c315f0490f4873
slahiff <sean243446@gmail.com>
2020-01-28 11:43:57 -0500

    Added User and Show resources to Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_163611) do
+ActiveRecord::Schema.define(version: 2020_01_28_164326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_01_28_163611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "show_id"
+    t.index ["show_id"], name: "index_reviews_on_show_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -56,5 +58,6 @@ ActiveRecord::Schema.define(version: 2020_01_28_163611) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "reviews", "shows"
   add_foreign_key "reviews", "users"
 end
