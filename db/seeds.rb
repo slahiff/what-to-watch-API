9659a25348cedb63a002a7ba85d17817f52392c5
slahiff <sean243446@gmail.com>
2020-01-30 13:30:15 -0500

    Added show_id to review controller and serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 Show.create([
               {
                 title: 'Stranger Things',
